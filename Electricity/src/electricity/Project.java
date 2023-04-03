9633444c5a556d0a03fa21623b31c097cd86498f
Rajiv <rajivrb2200@gmail.com>
2023-04-03 23:19:00 +0530

    Completed the Project file with necessary tags and menu buttons

--- Electricity/src/electricity/Project.java
+++ Electricity/src/electricity/Project.java
@@ -72,6 +72,34 @@ public class Project extends JFrame  implements ActionListener
         m3.addActionListener(this);
         m4.addActionListener(this);
         
+        
+        /* Second Column */
+        JMenu info = new JMenu("Information");
+        JMenuItem info1 = new JMenuItem("Update Information");
+        JMenuItem info2 = new JMenuItem("View Information");
+        
+        info.setForeground(Color.RED);
+        
+        /* ---- Pay Bill ---- */
+        info1.setFont(new Font("monospaced",Font.PLAIN,12));
+        ImageIcon icon41 = new ImageIcon(ClassLoader.getSystemResource("icon/icon4.png"));
+        Image image41 = icon41.getImage().getScaledInstance(20, 20,Image.SCALE_DEFAULT);
+        info1.setIcon(new ImageIcon(image41));
+        info1.setMnemonic('P');
+        info1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, ActionEvent.CTRL_MASK));
+        info1.setBackground(Color.WHITE);
+        
+        /* ---- Last Bill ----*/
+        info2.setFont(new Font("monospaced",Font.PLAIN,12));
+        ImageIcon icon42 = new ImageIcon(ClassLoader.getSystemResource("icon/icon6.png"));
+        Image image42 = icon42.getImage().getScaledInstance(20, 20,Image.SCALE_DEFAULT);
+        info2.setIcon(new ImageIcon(image42));
+        info2.setMnemonic('L');
+        info2.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_L, ActionEvent.CTRL_MASK));
+        info2.setBackground(Color.WHITE);
+        
+        info1.addActionListener(this);
+        info2.addActionListener(this);
         // Second Column
         
         JMenu user = new JMenu("User");
@@ -103,7 +131,7 @@ public class Project extends JFrame  implements ActionListener
         u1.addActionListener(this);
         u3.addActionListener(this);
         
-        
+       
         
         //Third Column
         
@@ -123,6 +151,179 @@ public class Project extends JFrame  implements ActionListener
         r1.setBackground(Color.WHITE);
         
         r1.addActionListener(this);
+        
+        
+        //Fourth Column
+        JMenu utility = new JMenu("Utility");
+        JMenuItem ut1 = new JMenuItem("Notepad");
+        JMenuItem ut2 = new JMenuItem("Calculator");
+        JMenuItem ut3 = new JMenuItem("Web Browser");
+        
+        utility.setForeground(Color.RED);
+        
+        
+        // Calender
+        ut1.setFont(new Font("Monospaced",Font.PLAIN,12));
+        ImageIcon icon8 = new ImageIcon(ClassLoader.getSystemResource("icon/icon.png"));
+        Image image8 = icon8.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
+        ut1.setIcon(new ImageIcon(image8));
+        ut1.setMnemonic('C');
+        ut1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
+        ut1.setBackground(Color.WHITE);
+        
+//        Calculator
+        ut2.setFont(new Font("monospcaced",Font.PLAIN,12));
+        ImageIcon icon9 = new ImageIcon(ClassLoader.getSystemResource("icon/icon9.png"));
+        Image image9 = icon9.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
+        ut2.setIcon(new ImageIcon(image9));
+        ut2.setMnemonic('X');
+        ut2.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X,ActionEvent.CTRL_MASK));
+        ut2.setBackground(Color.WHITE);
+        
+        
+//        web Browser
+        ut3.setFont(new Font("monospaced", Font.PLAIN,12));
+        ImageIcon icon10 = new ImageIcon(ClassLoader.getSystemResource("icon/icon10.png"));
+        Image image10 = icon10.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
+        ut3.setIcon(new ImageIcon(image10));
+        ut3.setMnemonic('W');
+        ut3.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W,ActionEvent.CTRL_MASK));
+        ut3.setBackground(Color.WHITE);
+        
+        
+        ut1.addActionListener(this);
+        ut2.addActionListener(this);
+        ut3.addActionListener(this);
+        
+        
+//        Fifth Column
+        JMenu exit = new JMenu("Logout");
+        JMenuItem ex = new JMenuItem("Logout");
+        exit.setForeground(Color.BLUE);
+        
+        
+//        Exit
+        ex.setFont(new Font("monospaced",Font.PLAIN,12));
+        ImageIcon icon11 = new ImageIcon(ClassLoader.getSystemResource("icon/icon11.png"));
+        Image image11 = icon11.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
+        ex.setIcon(new ImageIcon(image11));
+        ex.setMnemonic('Z');
+        ex.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z,ActionEvent.CTRL_MASK));
+        ex.setBackground(Color.WHITE);
+        ex.addActionListener(this);
+        
+        master.add(m1);
+        master.add(m2);
+        master.add(m3);
+        master.add(m4);
+        
+        info.add(info1);
+        info.add(info2);
+        
+        user.add(u1);
+        user.add(u3);
+        
+        report.add(r1);
+        
+        
+        utility.add(ut1);
+        utility.add(ut2);
+        utility.add(ut3);
+        
+        exit.add(ex);
+        
+        if(person.equals("Admin"))
+        {
+            mb.add(master);
+        }
+        else
+        {
+            mb.add(info);
+            mb.add(user);
+            mb.add(report);
+        }
+        mb.add(utility);
+        mb.add(exit);
+        
+        setJMenuBar(mb);
+        
+        setFont(new Font("Senserif",Font.BOLD,16));
+        setLayout(new FlowLayout());
+        setVisible(false);
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        String msg = ae.getActionCommand();
+        if(msg.equals("Customer Details"))
+        {
+            new CustomerDetails().setVisible(true);
+        }
+        else if(msg.equals("New Customer"))
+        {
+            new NewCustomer().setVisible(true);
+        }
+        else if(msg.equals("Calculate Bill"))
+        {
+            new CalculateBill().setVisible(true);
+        }
+        else if(msg.equals("Pay Bill"))
+        {
+            new PayBill(meter).setVisible(true);
+        }
+        else if(msg.equals("Notepad"))
+        {
+            try
+            {
+                Runtime.getRuntime().exec("notepad.exe");
+            }
+            catch(Exception e){ }
+        }
+        else if(msg.equals("Calculator"))
+        {
+            try{
+                Runtime.getRuntime().exec("calc.exe");
+            }
+            catch(Exception e) { }
+        }
+        else if(msg.equals("Web Browser"))
+        {
+            try{
+                Runtime.getRuntime().exec("C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe");
+            }
+            catch(Exception e){ }
+        }
+        else if(msg.equals("Logout"))
+        {
+            this.setVisible(true);
+            new Login().setVisible(true);
+        }
+        else if(msg.equals("Generate Bill"))
+        {
+            new GenerateBill(meter).setVisible(true);
+        }
+        else if(msg.equals("Deposite Details"))
+        {
+            new DepositeDetails().setVisible(true);
+        }
+        else if(msg.equals("view Information"))
+        {
+            new ViewInformation(meter).setVisible(true);
+        }
+        else if(msg.equals("Update Information"))
+        {
+            new UpdateInforamtion(meter).setVisible(true);
+        }
+        else if(msg.equals("Bill Details"))
+        {
+            new BillDetails(meter).setVisible(true);
+        }
+    }
+    
+    public static void main(String[] args)
+    {
+        new Project("","").setVisible(true);
     }
     
 }
