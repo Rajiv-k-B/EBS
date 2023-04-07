de968a4a4a674adcc36fd52088410965b823b067
Rajiv <rajivrb2200@gmail.com>
2023-04-07 22:51:06 +0530

    Deleted unwanted items

--- Electricity/src/electricity/CustomerDetails.java
+++ Electricity/src/electricity/CustomerDetails.java
@@ -1,28 +1,26 @@
 package electricity;
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.sql.*;
 
-public class CustomerDetails extends JFrame implements ActionListener
-{
+public class CustomerDetails extends JFrame implements ActionListener{
+ 
     JTable t1;
     JButton b1;
-    String x[] = {"Customer Name", "Meter Number", "Address","City","State","Email","Phone"};
+    String x[] = {"Customer Name","Meter Number","Address","City","State","Email","Phone"};
     String y[][] = new String[40][8];
-    int i=0,j=0;
+    int i=0, j=0;
     CustomerDetails(){
         super("Customer Details");
         setSize(1200,650);
         setLocation(400,150);
-
+        
         try{
-            Conn c1 = new Conn();
+            Conn c1  = new Conn();
             String s1 = "select * from customer";
-            ResultSet rs = c1.s.executeQuery(s1);
-            while(rs.next())
-            {
+            ResultSet rs  = c1.s.executeQuery(s1);
+            while(rs.next()){
                 y[i][j++]=rs.getString("name");
                 y[i][j++]=rs.getString("meter");
                 y[i][j++]=rs.getString("address");
@@ -34,28 +32,25 @@ public class CustomerDetails extends JFrame implements ActionListener
                 j=0;
             }
             t1 = new JTable(y,x);
-        }
-        catch(Exception e){
+            
+        }catch(Exception e){
             e.printStackTrace();
         }
-
+        
+        
         b1 = new JButton("Print");
         add(b1,"South");
         JScrollPane sp = new JScrollPane(t1);
         add(sp);
         b1.addActionListener(this);
     }
-
-    public void actionPerformed(ActionEvent ae)
-    {
+    public void actionPerformed(ActionEvent ae){
         try{
             t1.print();
-        }
-        catch(Exception e){ }
+        }catch(Exception e){}
     }
-
-    public static void main(String[] args)
-    {
+    
+    public static void main(String[] args){
         new CustomerDetails().setVisible(true);
     }
     
--- Swing/src/swing/Swing.java
+++ Swing/src/swing/Swing.java
@@ -2,13 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package electricity;
+package swing;
 
 /**
  *
  * @author baith
  */
-public class Electricity {
+import javax.swing.*;
+public class Swing {
 
     /**
      * @param args the command line arguments
